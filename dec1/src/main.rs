94d8b480768ff48479e7fe754b680f5a9423f729
GURA <gustav@gustavrasmussen.com>
2020-12-01 16:35:06 +0100

    solution to AoC1 part 1

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -2,12 +2,46 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let filename= "./test_input.txt";
-    let file = File::open(filename).expect("file not found!");
+    let filename= "./dec1_input_a.txt";
 
-    let reader = BufReader::new(file);
+    let input:Vec<i32> = input_to_i32vector(filename);
+
+
+    // Advent of Code Day 1 - Part 1
+    for i in &input {
+        for j in &input {
+            if check_sum_of_two(i,j,2020){
+                println!("{} * {} = {}",i,j, i*j);
+            }       
+        }
+    }
+
     
+
+}
+
+fn input_to_i32vector(s: &str) -> Vec<i32> {
+    let filename = s;
+    let file = File::open(filename).expect("file not found!");
+    let reader = BufReader::new(file);
+    let mut v: Vec<i32> = Vec::new();
+
     for line in reader.lines() {
-        println!("{}",line.unwrap());
+        
+        let a: i32 = line.unwrap().parse().expect("unable to parse");
+        v.push(a);
+    }
+
+    // Return the Vec<i32>
+    v
+
+}
+
+fn check_sum_of_two(a: &i32, b: &i32, x: i32) -> bool {
+    if a + b == x {
+        true
+    }
+    else {
+        false
     }
 }
