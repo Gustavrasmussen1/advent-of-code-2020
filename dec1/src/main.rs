69f4d413c4455213ade5c77c6f7e70a0764801de
GURA <gustav@gustavrasmussen.com>
2020-12-01 15:03:22 +0100

    test

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -1,15 +1,12 @@
 use std::fs::File;
-use std::io::{self,BufRead, BufReader};
-use std::env;
-use std::path::Path;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    //let dir = env::current_dir().unwrap();
     let filename= "./test_input.txt";
-    //let filepath = Path::to_str();
-    //let filepath = Path::new("./test_input.txt"); 
     let file = File::open(filename).expect("file not found!");
 
     let reader = BufReader::new(file);
+    
     for line in reader.lines() {
         println!("{}",line.unwrap());
     }
