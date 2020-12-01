662a080119e70a707dd89d7d6f6e2434f7d83244
GURA <gustav@gustavrasmussen.com>
2020-12-01 16:41:21 +0100

    solved AoC1 part 2

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -8,15 +8,26 @@ fn main() {
 
 
     // Advent of Code Day 1 - Part 1
-    for i in &input {
-        for j in &input {
-            if check_sum_of_two(i,j,2020){
-                println!("{} * {} = {}",i,j, i*j);
+    for a in &input {
+        for b in &input {
+            if check_sum_of_two(a,b,2020){
+                println!("{} * {} = {}",a,b, a*b);
             }       
         }
     }
 
-    
+    // Advent of Code Day 1 - Part 2
+
+    for a in &input {
+        for b in &input {
+            for c in &input {
+                if check_sum_of_three(a,b,c,2020){
+                    println!("{} * {} * {} = {}",a,b,c, a * b * c)
+                }
+            } 
+        }
+    }
+
 
 }
 
@@ -39,6 +50,15 @@ fn input_to_i32vector(s: &str) -> Vec<i32> {
 
 fn check_sum_of_two(a: &i32, b: &i32, x: i32) -> bool {
     if a + b == x {
+        true
+    }
+    else {
+        false
+    }
+}
+
+fn check_sum_of_three(a: &i32, b: &i32, c:&i32, x: i32) -> bool {
+    if a + b + c == x{
         true
     }
     else {
