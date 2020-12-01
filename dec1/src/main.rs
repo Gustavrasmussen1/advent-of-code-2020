675de01edd75be6f5bce6c608c3cb911aac55533
GURA <gura@defactoanalytics.io>
2020-12-01 14:55:48 +0100

    fixed io in rust

--- dec1/src/main.rs
+++ dec1/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::{self,BufRead, BufReader};
+use std::env;
+use std::path::Path;
 fn main() {
-    println!("Hello, world!2");
+    //let dir = env::current_dir().unwrap();
+    let filename= "./test_input.txt";
+    //let filepath = Path::to_str();
+    //let filepath = Path::new("./test_input.txt"); 
+    let file = File::open(filename).expect("file not found!");
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        println!("{}",line.unwrap());
+    }
 }
