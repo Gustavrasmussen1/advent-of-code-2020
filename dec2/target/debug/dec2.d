8dfa61415536a00d2d23e6e7ea3f222a519b317b
GURA <gustav@gustavrasmussen.com>
2020-12-02 09:48:49 +0100

    added day2

---
+++ dec2/target/debug/dec2.d
@@ -0,0 +1,1 @@
+C:\Users\GURA\Desktop\Github\Rust\advent-of-code-2020\dec2\target\debug\dec2.exe: C:\Users\GURA\Desktop\Github\Rust\advent-of-code-2020\dec2\src\main.rs
---
+++ dec2/target/debug/deps/dec2.d
@@ -0,0 +1,5 @@
+C:\Users\GURA\Desktop\Github\Rust\advent-of-code-2020\dec2\target\debug\deps\dec2.exe: src\main.rs
+
+C:\Users\GURA\Desktop\Github\Rust\advent-of-code-2020\dec2\target\debug\deps\dec2.d: src\main.rs
+
+src\main.rs:
