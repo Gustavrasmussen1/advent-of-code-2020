7874cb66082af45bb4abb8708dbb7ddca7293b30
GURA <gustav@gustavrasmussen.com>
2020-12-05 17:54:06 +0100

    solved day 5

--- dec5/src/main.rs
+++ dec5/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
+//use std::collections::HashMap;
 use std::ops::{Bound, RangeBounds};
 
 // Source: https://users.rust-lang.org/t/how-to-get-a-substring-of-a-string/1351/11 - User carlomilanesi
@@ -50,19 +50,83 @@ impl StringUtils for str {
 
 fn main() {
 
-    let filename = "./dec5_test_input.txt";
-    decode_boardingpass(filename);
+    let filename = "./dec5_input.txt";
+    let file = File::open(filename).expect("file not found!");
+    let reader = BufReader::new(file);
+    
+    let mut input:Vec<String> = Vec::new();
+    let mut seat_ids:Vec<i32> = Vec::new();
+    input = reader.lines().map(|x| x.unwrap()).collect();
+    
+    seat_ids = input.iter().map(|x| decode_seat_id(x)).collect();
+
+    println!("MAX: {:?}",seat_ids.iter().max());
+
+    //seat_ids = seat_ids.iter().map(|seat| seat_ids.contains(seat)).collect();
+    let mut my_seat:Vec<bool> = seat_ids.iter().map(|&seat_id| !seat_ids.contains(&(&seat_id+1)) && !seat_ids.contains(&(&seat_id-1))).collect();
+
+    let mut a = 0;
+    let mut b = 0;
+    for id in seat_ids.iter() {
+
+        a = id - 1;
+        b = id + 1;
+
+        if seat_ids.contains(&a) && seat_ids.contains(&b) {
+            
+        }
+        else{
+            println!("{}", id);
+        }
+
+
+    }
+    //println!("{:?}", my_seat);
+}
+
+fn decode_seat_id(s: &str) -> i32{
+
+    let seat = decode_boardingpass(s);
+    //let mut row = (0,127);
+    //let mut column = (0,7);
+    let id = seat.0 * 8 + seat.1;
+    id
 
 }
 
-fn decode_boardingpass(s: &str){
+fn decode_boardingpass(s:&str) ->(i32,i32) {
+    let mut row = (0,127);
+    let mut column = (0,7);
 
-    let filename = s;
-    let file = File::open(filename).expect("file not found!");
-    let reader = BufReader::new(file);
+    let mut seat = String::from(s);
 
-    for line in reader.lines() {
-        println!("{}",line.unwrap());
+    for c in seat.chars(){
+        //println!("{}", c);
+        match c.to_string().substring(0,1) {
+            "F" => {
+                //println!("Row from: {}-{}",row.0, row.1);
+                row.1 -= (row.1 - row.0)/ 2 + 1;
+                //println!("Row to: {}-{}",row.0, row.1);
+            },
+            "B" => {
+                //println!("Row from: {}-{}",row.0, row.1);
+                row.0 += (row.1-row.0)/2 + 1;
+                //println!("Row to: {}-{}",row.0, row.1);
+            },
+            "R" => {
+                //println!("Column from: {}-{}",column.0, column.1);
+                column.0 += (column.1-column.0)/2 + 1;
+                //println!("Column to: {}-{}",column.0, column.1);
+            },
+            "L" => {
+                //println!("Column from: {}-{}",column.0, column.1);
+                column.1 -= (column.1-column.0) / 2 + 1;
+                //println!("Column to: {}-{}",column.0, column.1);
+            },
+            _ => {}
         }
-        
     }
+
+    //println!("{} {}",row.0, column.1);
+    (row.0,column.1)
+}
